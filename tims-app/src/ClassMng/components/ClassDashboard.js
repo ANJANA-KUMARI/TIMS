7cf6377ea35ac51e2dd13b53112d3d1f2315703c
Nuwan Karunarathna <nuwankarunarathna@hotmail.com>
2019-08-20 17:02:03 +0530

    [FEAT] Added AddSubjectPopup ui

--- tims-app/src/ClassMng/components/ClassDashboard.js
+++ tims-app/src/ClassMng/components/ClassDashboard.js
@@ -1,52 +1,81 @@
 import React, { Component } from 'react';
 import { Card, CardHeader } from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
-import "../ClassMng.css";
+import '../ClassMng.css';
+import AddSubject from './AddSubject';
 
 class ClassDashboard extends Component {
-    state = {}
-    render() {
-        return (
-            <div className="shortcut-wrap">
-                <Card className="class-dashboard-shortcut">
-                    <CardHeader
-                        avatar={
-                            <Avatar>
-                                <img className="w-100" src="/image/subject.svg" />
-                            </Avatar>
-                        }
-                        title="Subjects"
-                        subheader="6">
-                    </CardHeader>
-                </Card>
+  state = {
+    showAddSubjectForm: false
+  };
 
-                <Card className="class-dashboard-shortcut">
-                    <CardHeader
-                        avatar={
-                            <Avatar>
-                                <img className="w-100 h-100" src="/image/class.svg" />
-                            </Avatar>
-                        }
-                        title="Classes"
-                        subheader="10">
-                    </CardHeader>
-                </Card>
+  toggleAddSubjectForm = () => {
+    console.log('Yaluuuuuuuuuuuuuu);');
+    this.setState({
+      showAddSubjectForm: !this.state.showAddSubjectForm
+    });
+  };
 
-                <Card className="class-dashboard-shortcut">
-                    <CardHeader
-                        avatar={
-                            <Avatar>
-                                <img className="w-100 h-100" src="/image/class.svg" />
-                            </Avatar>
-                        }
-                        title="Add Subject"
-                        subheader="10">
-                    </CardHeader>
-                </Card>
+  render() {
+    return (
+      <div className="shortcut-wrap">
+        <Card className="class-dashboard-shortcut">
+          <CardHeader
+            avatar={
+              <Avatar>
+                <img className="w-100" src="/image/subject.svg" />
+              </Avatar>
+            }
+            title="Subjects"
+            subheader="6"
+          />
+        </Card>
 
-            </div>
-        );
-    }
+        <Card className="class-dashboard-shortcut">
+          <CardHeader
+            avatar={
+              <Avatar>
+                <img className="w-100 h-100" src="/image/class.svg" />
+              </Avatar>
+            }
+            title="Classes"
+            subheader="10"
+          />
+        </Card>
+
+        <Card className="class-dashboard-shortcut">
+          <CardHeader
+            avatar={
+              <Avatar>
+                <img className="w-100 h-100" src="/image/class.svg" />
+              </Avatar>
+            }
+            title="Add Subject"
+          />
+        </Card>
+
+        <Card
+          onClick={this.toggleAddSubjectForm}
+          className="class-dashboard-shortcut"
+        >
+          <CardHeader
+            avatar={
+              <Avatar>
+                <img className="w-100 h-100" src="/image/class.svg" />
+              </Avatar>
+            }
+            title="Add Class"
+          />
+        </Card>
+
+        {this.state.showAddSubjectForm ? (
+          <AddSubject onCancelPopup={this.toggleAddSubjectForm} />
+        ) : (
+          ''
+        )}
+      </div>
+    );
+  }
 }
 
 export default ClassDashboard;
