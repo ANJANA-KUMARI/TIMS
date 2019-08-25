2adfb0e1b5363ff70a2a9f866795ec1d216d7432
Nuwan Karunarathna <nuwankarunarathna@hotmail.com>
2019-08-25 15:01:43 +0530

    [FEAT/UP] Design Homepage

--- tims-app/src/components/HomePage.js
+++ tims-app/src/components/HomePage.js
@@ -1,90 +1,89 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { ROUTE_PATHS } from '../Constants';
 import DashboardCard from './DashboardCard';
 
 export default function HomePage() {
+  const dashboardCards = [
+    {
+      id: 1,
+      image: '/image/classes.svg',
+      title: 'Classes',
+      page: ROUTE_PATHS.CLASSES
+    },
+    {
+      id: 2,
+      image: '/image/attendance.svg',
+      title: 'Attendance',
+      page: ROUTE_PATHS.ATTENDENCE
+    },
+    {
+      id: 3,
+      image: '/image/student.svg',
+      title: 'Students',
+      page: ROUTE_PATHS.STUDENTS
+    },
+    {
+      id: 4,
+      image: '/image/employee.svg',
+      title: 'Employees',
+      page: ROUTE_PATHS.EMPLOYEES
+    },
+    {
+      id: 5,
+      image: '/image/parent.svg',
+      title: 'Parents',
+      page: ROUTE_PATHS.PARENTS
+    },
+    {
+      id: 6,
+      image: '/image/fee.svg',
+      title: 'Fee',
+      page: ROUTE_PATHS.FEE
+    },
 
-    const dashboardCards = [
-        {
-            id: 1,
-            image: "/image/classes.svg",
-            title: "Classes",
-            page: ROUTE_PATHS.CLASSES
-        },
-        {
-            id: 2,
-            image: "/image/classes.svg",
-            title: "Attendance",
-            page: ROUTE_PATHS.ATTENDENCE
-        },
-        {
-            id: 3,
-            image: "/image/classes.svg",
-            title: "Students",
-            page: ROUTE_PATHS.STUDENTS
-        },
-        {
-            id: 4,
-            image: "/image/classes.svg",
-            title: "Employees",
-            page: ROUTE_PATHS.EMPLOYEES
-        },
-        {
-            id: 5,
-            image: "/image/classes.svg",
-            title: "Parents",
-            page: ROUTE_PATHS.PARENTS
-        },
-        {
-            id: 6,
-            image: "/image/classes.svg",
-            title: "Fee",
-            page: ROUTE_PATHS.FEE
-        },
-
-        {
-            id: 7,
-            image: "/image/classes.svg",
-            title: "TimeTable",
-            page: ROUTE_PATHS.TIMETABLE
-        },
-        {
-            id: 8,
-            image: "/image/classes.svg",
-            title: "Study Material",
-            page: ROUTE_PATHS.STUDY_MATERIAL
-        }
-    ]
-
-    const DashboardItemListStyle = {
-        innerWrap: {
-
-            display: "grid",
-            gridTemplateColumns: "25% 25% 25% 25%",
-            padding: "20px",
-            width: '100%'
-        },
-        outerWrap: {
-            height: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-        }
+    {
+      id: 7,
+      image: '/image/timetable.svg',
+      title: 'TimeTable',
+      page: ROUTE_PATHS.TIMETABLE
+    },
+    {
+      id: 8,
+      image: '/image/studyMat.svg',
+      title: 'Study Material',
+      page: ROUTE_PATHS.STUDY_MATERIAL
     }
+  ];
 
-    return (
-        <div style={DashboardItemListStyle.outerWrap}>
+  const DashboardItemListStyle = {
+    innerWrap: {
+      display: 'grid',
+      gridTemplateColumns: '25% 25% 25% 25%',
+      padding: '20px',
+      width: '100%'
+    },
+    outerWrap: {
+      height: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  };
 
-            <div style={DashboardItemListStyle.innerWrap}>
-                {
-                    dashboardCards.map((dashboardCard) => {
-                        return <DashboardCard key={dashboardCard.id} image={dashboardCard.image} title={dashboardCard.title} page={dashboardCard.page} />
-                    })
-                }
-            </div>
-        </div>
-
-    );
+  return (
+    <div style={DashboardItemListStyle.outerWrap}>
+      <div style={DashboardItemListStyle.innerWrap}>
+        {dashboardCards.map(dashboardCard => {
+          return (
+            <DashboardCard
+              key={dashboardCard.id}
+              image={dashboardCard.image}
+              title={dashboardCard.title}
+              page={dashboardCard.page}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
-
